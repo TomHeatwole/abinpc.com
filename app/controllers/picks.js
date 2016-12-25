7e125d798e57871faa08df30a881cea3530b443a
Tom <tgh8xn@virginia.edu>
2016-12-25 14:01:29 -0500

    Shell for view picks function

---
+++ app/controllers/picks.js
@@ -0,0 +1,53 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  A: '',
+  B: '',
+  C: '', 
+  D: '',
+  F: 'Final Four',
+
+  ASelected: false,
+  BSelected: false,
+  CSelected: false,
+  DSelected: false,
+  FSelected: false,
+
+  actions: {
+    selectA() {
+      this.set('ASelected', true);
+      this.set('BSelected', false);
+      this.set('CSelected', false);
+      this.set('DSelected', false);
+      this.set('FSelected', false);
+    },
+    selectB() {
+      this.set('ASelected', false);
+      this.set('BSelected', true);
+      this.set('CSelected', false);
+      this.set('DSelected', false);
+      this.set('FSelected', false);
+    },
+    selectC() {
+      this.set('ASelected', false);
+      this.set('BSelected', false);
+      this.set('CSelected', true);
+      this.set('DSelected', false);
+      this.set('FSelected', false);
+    },
+    selectD() {
+      this.set('ASelected', false);
+      this.set('BSelected', false);
+      this.set('CSelected', false);
+      this.set('DSelected', true);
+      this.set('FSelected', false);
+    },
+    selectF() {
+      this.set('ASelected', false);
+      this.set('BSelected', false);
+      this.set('CSelected', false);
+      this.set('DSelected', false);
+      this.set('FSelected', true);
+    }
+  }
+});
--- app/routes/picks.js
+++ app/routes/picks.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  //TODO: Setup controller
+  // - Make sure all of the 'selected' variables are initialized to false
 });
